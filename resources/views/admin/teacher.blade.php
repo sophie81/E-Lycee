0b4d56c8af26dfdc27a8c71233270fb8cd160aef
DalounyS <dalouny.sn@gmail.com>
2016-07-27 11:43:21 +0200

    teacher blade

--- resources/views/admin/teacher.blade.php
+++ resources/views/admin/teacher.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.admin')
 
 @section('content')
-
+    <nav>
+        <li><a href="{{url('post','create')}}">Créer un article</a></li>
+        <li><a href="{{url('logout')}}">Se déconnecter</a></li>
+    </nav>
+    @if(Session::has('message'))
+        <p class="msg">{{Session::get('message')}}</p>
+    @endif
     <p> Page accueil teacher</p>
 @endsection
