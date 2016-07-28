6976bee814f6fc750867c513869684d8232c5351
Sophie <sophie.riviere81@gmail.com>
2016-07-28 21:18:16 +0200

    reponse

--- resources/views/admin/choice/create.blade.php
+++ resources/views/admin/choice/create.blade.php
@@ -3,15 +3,17 @@
     @if(Session::has('message'))
         <p class="msg">{{Session::get('message')}}</p>
     @endif
-    <h1 class="h1-form">Entrez les réponses !</h1>
+    <h1 class="h1-form">Entrez vos réponses !</h1>
     <nav>
         @include('partials.teacherNav')
     </nav>
+    <h3>Question :</h3>
+    <p>{{$question->content}}</p>
     <form method="POST" action="{{url('choice')}}" class="form-create">
         {{csrf_field()}}
         @forelse($question->choices as $index => $choice)
         <div class="title">
-            <label>Question {{$index + 1}} : </label>
+            <label>Réponse {{$index + 1}} : </label>
             <textarea name="content">{{old('content')}}</textarea>
             @if($errors->has('content'))
                 <p><span class="error">{{$errors->first('content')}}</span></p>
@@ -19,7 +21,7 @@
         </div>
         <div>
             <input type="checkbox" name="status" id="cbox1" value="true"> <label for="cbox1">Vrai</label>
-            <input type="checkbox" name="status" id="cbox2" value="false"> <label for="cbox2">aux</label>
+            <input type="checkbox" name="status" id="cbox2" value="false"> <label for="cbox2">Faux</label>
         </div>
         @empty
             <p>pas de reponse</p>
