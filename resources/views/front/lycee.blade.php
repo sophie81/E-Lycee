c2c85369e4f725b5ec20df4d042a67dc25714d56
DalounyS <dalouny.sn@gmail.com>
2016-07-29 14:15:21 +0200

    page statique

--- resources/views/front/lycee.blade.php
+++ resources/views/front/lycee.blade.php
@@ -3,10 +3,23 @@
 @section('title', $title)
 
 @section('content')
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris felis ante, tincidunt sit amet nulla sit amet, vehicula dictum augue. Aenean fermentum leo sed sodales sodales. Etiam facilisis tellus sed metus viverra dignissim. Duis vel viverra neque. Fusce consequat tincidunt leo sit amet maximus. Sed ipsum urna, lobortis non nisl et, suscipit consequat risus. Cras a eleifend massa, sit amet accumsan sapien. Praesent lacinia est ac lorem maximus, rutrum tincidunt lectus maximus. Aenean consequat lacus non augue tempor pretium. Pellentesque congue vestibulum mauris quis molestie. In venenatis ligula at enim placerat, blandit scelerisque neque aliquam. Duis scelerisque malesuada purus, ac ultricies tortor gravida ut. Vivamus a sapien tempor, vulputate purus sed, auctor ligula. Aenean purus est, rutrum eu venenatis non, consectetur sed tellus.
+	<div class="col-xs-12">
+		<h2>Ecole Multimedia</h2>
+		<img src="{{url('images/em.jpg')}}" alt="Ecole multimedia">
+	    <h3>L'apprenant, au centre de nos préoccupations </h3>
 
-        Donec varius mollis purus quis facilisis. Morbi congue rutrum sagittis. Donec arcu sem, ultrices ut venenatis at, iaculis in sapien. Morbi rutrum tortor eget nisi bibendum euismod. Sed condimentum lacus in purus imperdiet tristique. Donec in purus tellus. Quisque porta laoreet sagittis.
 
-        Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent eu sem suscipit, facilisis ante ut, luctus sapien. Fusce vulputate auctor mi non bibendum. Donec et dignissim arcu. Vivamus posuere, neque vitae mollis rhoncus, nulla metus luctus purus, sit amet vulputate tellus nibh eget est. Cras a finibus lacus, at vulputate orci. Vestibulum neque erat, pretium at fringilla eu, tristique ac enim. Aliquam erat volutpat. Praesent non eleifend justo. Vestibulum sagittis massa quis purus lobortis, ut hendrerit tellus tincidunt. In ultricies libero elit, sed suscipit augue placerat eget. Nullam interdum suscipit orci ut porta. Vestibulum facilisis dui hendrerit ante aliquet mollis.</p>
+		<h4>Privilégier l'humain</h4>
+		<p>Nos formations sont accessibles au plus grand nombre grâce à des frais de scolarité étudiés au plus près. L’école propose l’alternance dès que possible dans le parcours scolaire pour accéder aux diplômes tout en étant salarié. C’est une école à taille humaine qui se démarque par l’importance accordée aux valeurs pédagogiques et morales de sa mission.</p>
+
+		<h4>Le contrat de la réussite</h4>
+		<p>Faire ses études à L’École Multimédia, c’est adhérer à un contrat qui nous oblige à un seul objectif : la réussite de chacun.Tout au long de sa scolarité, l’élève est accompagné par une équipe disponible, dont la vocation est de ne laisser personne sur le bord de la route. Nos enseignants sont attachés à leur école et privilégie un rapport direct avec les élèves.</p>
+		
+		<h4>L’emploi au bout de la scolarité</h4>
+		<p>Toutes nos filières de formation conduisent à un recrutement. Les études se concluent par une ou deux années en alternance dans le cadre du contrat de professionnalisation. Les étudiants ne sortent pas seulement avec un diplôme, ils partent avec un emploi et des compétences fondamentales qui les porteront tout au long de leur vie professionnelle.</p>
+
+		<h4>L’indépendance, une garantie éthique</h4>
+		<p>L’École Multimédia n’appartient à aucun groupe financier ni industriel. Elle est dirigée par ses fondateurs, des professionnels de la communication écrite et visuelle, qui considèrent leurs étudiants comme de futurs confrères. Elle en est solidaire. Elle les forme techniquement et intellectuellement comme les salariés libres de demain.</p>
+    </div>
 @endsection
 
--- resources/views/front/mentions.blade.php
+++ resources/views/front/mentions.blade.php
@@ -3,9 +3,24 @@
 @section('title', $title)
 
 @section('content')
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris felis ante, tincidunt sit amet nulla sit amet, vehicula dictum augue. Aenean fermentum leo sed sodales sodales. Etiam facilisis tellus sed metus viverra dignissim. Duis vel viverra neque. Fusce consequat tincidunt leo sit amet maximus. Sed ipsum urna, lobortis non nisl et, suscipit consequat risus. Cras a eleifend massa, sit amet accumsan sapien. Praesent lacinia est ac lorem maximus, rutrum tincidunt lectus maximus. Aenean consequat lacus non augue tempor pretium. Pellentesque congue vestibulum mauris quis molestie. In venenatis ligula at enim placerat, blandit scelerisque neque aliquam. Duis scelerisque malesuada purus, ac ultricies tortor gravida ut. Vivamus a sapien tempor, vulputate purus sed, auctor ligula. Aenean purus est, rutrum eu venenatis non, consectetur sed tellus.
+	<div class="col-xs-12">
+		<h2>Mentions Légales</h2>
+		<h4>Droits d’auteur – Copyright</h4>
+		<p>Tous les droits de reproduction sont réservés, y compris les représentations photographiques.</p>
+		<p>La reproduction de tout ou partie du site est formellement interdite sauf autorisation expresse du directeur de la publication.</p>
 
-        Donec varius mollis purus quis facilisis. Morbi congue rutrum sagittis. Donec arcu sem, ultrices ut venenatis at, iaculis in sapien. Morbi rutrum tortor eget nisi bibendum euismod. Sed condimentum lacus in purus imperdiet tristique. Donec in purus tellus. Quisque porta laoreet sagittis.
+		<h4>Avertissement</h4>
+		<p>Les informations qui apparaissent sur ce site sont données à titre purement indicatif. L’Ecole Multimédia met tout en œuvre pour assurer l’exactitude des informations diffusées sur son site à la date de leur insertion ainsi que leur mise à jour régulière. Néanmoins, l’Ecole Multimédia décline toute responsabilité quant à l’exclusivité et l’utilisation par toute personne physique ou morale des informations fournies.</p>
 
-        Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent eu sem suscipit, facilisis ante ut, luctus sapien. Fusce vulputate auctor mi non bibendum. Donec et dignissim arcu. Vivamus posuere, neque vitae mollis rhoncus, nulla metus luctus purus, sit amet vulputate tellus nibh eget est. Cras a finibus lacus, at vulputate orci. Vestibulum neque erat, pretium at fringilla eu, tristique ac enim. Aliquam erat volutpat. Praesent non eleifend justo. Vestibulum sagittis massa quis purus lobortis, ut hendrerit tellus tincidunt. In ultricies libero elit, sed suscipit augue placerat eget. Nullam interdum suscipit orci ut porta. Vestibulum facilisis dui hendrerit ante aliquet mollis.</p>
+		<h4>Législation</h4>
+		<p>Conformément à la loi du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, vous disposez d’un droit d’accès, de rectification et de suppression des informations vous concernant. Pour l’exercer, adressez-vous à : informations@ecole-multimedia.com</p>
+		<p>Ce site est protégé par la loi du 1er juillet 1998 sur la protection juridique des bases de données et de tous ses éléments, tel que les textes, les logos, les graphismes, les images et photos, qui sont la propriété exclusive de L’Ecole Multimédia.</p>
+
+		<p>Toute utilisation, y compris la reproduction même partielle, la modification, la distribution, la transmission, l’affichage du contenu ou de l’un quelconque des éléments de ce site est strictement interdite sans accord écrit L’Ecole Multimédia. Ces éléments sont protégés par la loi française et les lois internationales de protection de la propriété intellectuelle, et notamment les articles Art L.335-2 et L335-3 du code de la propriété intellectuelle. Tout contrevenant à cette protection fera l’objet de poursuites.</p>
+
+		<p>Sauf indications contraires (documents libres de droits), si vous souhaitez utiliser des photos ou 
+		des textes sur ce site, vous devez nous contacter à : informations@ecole-multimedia.com</p>
+
+		<p>Par ailleurs, l’utilisation des informations contenues sur le présent site relève de la seule responsabilité de l’utilisateur. Nous ne pourrions en aucun cas, et pour quelque cause que ce soit, en être tenus pour responsables, et ce, quelles qu’en soient les conséquences. Nous ne sommes responsables d’aucune erreur ou omission sur le présent site.</p>
+	</div>
 @endsection
